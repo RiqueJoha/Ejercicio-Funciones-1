35e6a345be7b307c88ea10124c03a7b7ad9453e1
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-04-26 21:49:40 -0300

    correccion

--- funciones.js
+++ funciones.js
@@ -16,22 +16,39 @@ const ENLACE_3="https://es.stackoverflow.com";
 let opcionRecibida = prompt("Ingrese una opcion estetica: 1, 2 o 3");
 let usuarioingresado= prompt ("Ingrese su nombre de usuario");
 
+usuario.innerHTML=usuarioingresado;
+
+const IMG_TAM_1 = "200px";
+const IMG_TAM_2 = "300px";
+const IMG_TAM_3 = "450px";
+
+const TXT_TAM_1 = "15px";
+const TXT_TAM_2 = "20px";
+const TXT_TAM_3 = "30px";
+
+const TXT_FUENTE_1 = "monospace";
+const TXT_FUENTE_2 = "cursive";
+const TXT_FUENTE_3 = "fantasy";
+
+const TXT_USUARIO_COLOR_1 = "red";
+const TXT_USUARIO_COLOR_2 = "violet";
+const TXT_USUARIO_COLOR_3 = "white";
+
+
 function modificarPagina(colorFondo,colorTexto,colorUsuario,tamanioTexto,tipoFuente,enlaceImg,anchoImg, enlacePagina) {
 cuerpoPagina.style.background=colorFondo;
 cuerpoPagina.style.color=colorTexto;
 usuario.style.color=colorUsuario;
 cuerpoPagina.style.fontSize=tamanioTexto;1
-titulo.style.fontFamily=tipoFuente;
+cuerpoPagina.style.fontFamily=tipoFuente;
 imagen.src=enlaceImg;
 imagen.style.width=anchoImg;
 enlace.href=enlacePagina;  
 }
 
-1
 if (opcionRecibida == 1) {
-    modificarPagina("orange", "black", "red", "15px", "monospace" , IMG_1 , "200px" ,  ENLACE_1);
+modificarPagina("orange", "black", TXT_USUARIO_COLOR_1, TXT_TAM_1, TXT_FUENTE_1 , IMG_1 , IMG_TAM_1 ,  ENLACE_1);
 } else if (opcionRecibida == 2) {
-    modificarPagina("black", "white", "purple", "20px", "cursive",IMG_2, "300px" , ENLACE_2);
+modificarPagina("black", "white", TXT_USUARIO_COLOR_2,TXT_TAM_2, TXT_FUENTE_2,IMG_2, IMG_TAM_2 , ENLACE_2);
 } else if (opcionRecibida == 3) {
-    modificarPagina("pink", "green", "white", "30px", "fantasía", IMG_3,"450px" , ENLACE_3);
-}
+modificarPagina("pink", "green", TXT_USUARIO_COLOR_3, TXT_TAM_3, TXT_FUENTE_3, IMG_3,IMG_TAM_3 , ENLACE_3);}
